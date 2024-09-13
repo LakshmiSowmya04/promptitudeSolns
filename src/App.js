ee61271fc4ec2649702afe444c21137d5f32130f
drish2126 <drishtigurnani2126@gmail.com>
2024-09-13 20:52:25 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Nav from "./components/NavBar";
-import Body from "./components/Body";
-import MyAccount from "./components/MyAccount";
-import Reminder from "./components/Remainder";
-import Home from "./components/Home";
 
 function App() {
   return (
