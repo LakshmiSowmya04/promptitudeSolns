0ee30ea897deddd50df4eb1801f4733eb258dd50
HENILBHUVA <henil.bhuva500@gmail.com>
2024-09-13 22:48:06 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,33 @@
+// import React from 'react';
+// import './Header.css';
+
+// function Header() {
+//   return (
+//     <div className="header">
+//       <h1>Promptitude</h1>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
 import React from 'react';
+import { FaUser, FaBell } from 'react-icons/fa'; // Import icons from react-icons
+import { Link } from 'react-router-dom'; // Import Link for routing
 import './Header.css';
 
 function Header() {
   return (
     <div className="header">
       <h1>Promptitude</h1>
+      <div className="header-icons">
+        <Link to="/notifications">
+          <FaBell className="icon" />
+        </Link>
+        <Link to="/profile">
+          <FaUser className="icon" />
+        </Link>
+      </div>
     </div>
   );
 }
