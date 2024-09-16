92d11a661d0b14a7011f5c3c056e4e027968c921
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2024-09-16 17:33:55 +0530

    changed ui and api calling changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Pages/Dashboard';
-import Tasks from './components/Pages/Tasks';
-import Clients from './components/Pages/Clients';
-import Vaults from './components/Pages/Vaults';
-import Reminder from './components/Pages/Reminder';
-import MyAccount from './components/Pages/MyAccount';
-import Members from './components/Pages/Teams/Members'; // Importing the Members component
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Pages/Dashboard";
+import Tasks from "./components/Pages/Tasks";
+import Clients from "./components/Pages/Clients";
+import Vaults from "./components/Pages/Vaults";
+import Reminder from "./components/Pages/Reminder";
+import MyAccount from "./components/Pages/MyAccount";
+import Members from "./components/Pages/Teams/Members"; // Importing the Members component
+import "./App.css";
 
 function App() {
   return (
@@ -26,7 +26,8 @@ function App() {
               <Route path="/vaults" element={<Vaults />} />
               <Route path="/reminder" element={<Reminder />} />
               <Route path="/my-account" element={<MyAccount />} />
-              <Route path="/my-teams/members" element={<Members />} /> {/* Routing for Members */}
+              <Route path="/my-teams/members" element={<Members />} />{" "}
+              {/* Routing for Members */}
             </Routes>
           </div>
         </div>
